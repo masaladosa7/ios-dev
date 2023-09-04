f33b9245205e7abe4e4b65f8751e5cdda4cdc0b4
Arya Devaskar <62664771+masaladosa7@users.noreply.github.com>
2023-09-04 12:54:22 +0530

    Uploaded I am rich

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Arya on 28/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
